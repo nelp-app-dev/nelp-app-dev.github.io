cbe65c9f0f99a9958b22afb1d31c992f07f1b792
nelp <nelp.app.dev@gmail.com>
2021-06-02 17:12:08 -0400

    Updates

--- src/features/Loading/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import './Loading.css';
-
-export const Loading = (props: any) => {
-  return props.loading ? (
-    <div className="loading-container">
-      <div className="loading" />
-    </div>
-  ) : null;
-};
